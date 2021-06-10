7debd256a0435522556ba3b30a6b115b1b4becbb
Harrf Akbar <harrf_akbar@yahoo.com>
2021-06-10 13:49:28 -0700

    changed forecast data

--- utils/forecast.js
+++ utils/forecast.js
@@ -11,7 +11,8 @@ const forecast = (lat, long, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, 'The weather is currently: ' + body.current.temperature + ' degrees' )
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature +
+            ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%')
         }
     })
 }
